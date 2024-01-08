97f3b77d5c300195d1e5ca75f3ba551c1e491e94
Rahul Yadav <rahulr20y@gmail.com>
2024-01-08 20:17:08 +0530

    initialization

---
+++ src/main/java/com/practice/learningjava/LearningjavaApplication.java
@@ -0,0 +1,13 @@
+package com.practice.learningjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/learningjava/LearningjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.learningjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
